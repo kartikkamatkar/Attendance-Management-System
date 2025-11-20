772873bb1997c945efb2370136eaea5c11306759
jfdkartik <KartikKamatkar20@gmail.com>
2025-11-20 23:57:30 +0530

    update file

--- src/main/java/com/example/attendancefr/AttendanceManagementSystemApplication.java
+++ src/main/java/com/example/attendancefr/AttendanceManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AttendanceManagementSystemApplication {
-
+//this is the is for attendancance mangement system is for student as well as collage
 	public static void main(String[] args) {
 		SpringApplication.run(AttendanceManagementSystemApplication.class, args);
 	}
